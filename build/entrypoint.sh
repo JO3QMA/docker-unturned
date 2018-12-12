c43ec07b5b1ec39c7974fd79f94d9cd93204f482
JO3QMA <g.dark.matter@gmail.com>
2018-12-12 12:38:07 +0900

    first commit.

---
+++ build/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ -f $PWD/Unturned.x86 ]; then
+    echo "Unturned.x86 : Found."
+else
+    echo "Unturned.x86 : Not Found."
+    /srv/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 32  +login ${ID} ${PASS} +force_install_dir ${UNT_DIR} +app_update 304930 validate +exit
+fi
+
+if [ -f $PWD/RocketLauncher.exe ]; then
+    echo "RocketLauncher.exe : Found."
+    ./update.sh ${ID} ${PASS}
+    ./start.sh ${INSTANCE}
+else
+    echo "RocketLauncher.exe : Not Found."
+    echo "RocketLauncher.exe : Downloading..."
+    curl  -O https://ci.rocketmod.net/job/Rocket.Unturned/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip 
+    unzip -o Rocket.zip
+    rm -f Rocket.zip
+    chmod +x ${UNT_DIR}/Scripts/Linux/*.sh
+    mv ${UNT_DIR}/Scripts/Linux/*.sh ${UNT_DIR}/
+    mv ${UNT_DIR}/Scripts/Linux/*.exe ${UNT_DIR}/
+    ./update.sh ${ID} ${PASS}
+    ./start.sh ${INSTANCE}
+fi
