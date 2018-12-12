ba72e31d8224229f4f7002f17cfce6df8657bf4c
JO3QMA <g.dark.matter@gmail.com>
2018-12-13 06:59:53 +0900

    fix typo

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [ -f $PWD/Unturned.x86 ]; then
+if [ -e ${UNT_DIR}/Unturned.x86 ]; then
     echo "Unturned.x86 : Found."
 else
     echo "Unturned.x86 : Not Found."
     /srv/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 32  +login ${ID} ${PASS} +force_install_dir ${UNT_DIR} +app_update 304930 validate +exit
 fi
 
-if [ -f $PWD/RocketLauncher.exe ]; then
+if [ -e ${UNT_DIR}/RocketLauncher.exe ]; then
     echo "RocketLauncher.exe : Found."
     ./update.sh ${ID} ${PASS}
     ./start.sh ${INSTANCE}
@@ -14,7 +14,7 @@ else
     echo "RocketLauncher.exe : Not Found."
     echo "RocketLauncher.exe : Downloading..."
     curl  -O https://ci.rocketmod.net/job/Rocket.Unturned/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip 
-    unzip -o Rocket.zip
+    unzip -o Rocket.zip -d ${UNT_DIR}
     rm -f Rocket.zip
     chmod +x ${UNT_DIR}/Scripts/Linux/*.sh
     mv ${UNT_DIR}/Scripts/Linux/*.sh ${UNT_DIR}/
