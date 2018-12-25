f5c9d383071fa854c800ce8fe86a1edeea361fec
JO3QMA <g.dark.matter@gmail.com>
2018-12-25 10:42:35 +0900

    Delete duplicate processing

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-if [ -e ${UNT_DIR}/Unturned.x86 ]; then
-    echo "Unturned.x86 : Found."
-else
-    echo "Unturned.x86 : Not Found."
-    /srv/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 32  +login ${ID} ${PASS} +force_install_dir ${UNT_DIR} +app_update 304930 validate +exit
-fi
+echo "Update Unturned Server..."
+/srv/steamcmd/steamcmd.sh \
+    +@sSteamCmdForcePlatformBitness 32  \
+    +login ${ID} ${PASS} \
+    +force_install_dir ${UNT_DIR} \
+    +app_update 304930 \
+    validate \
+    +exit
 
 if [ -e ${UNT_DIR}/RocketLauncher.exe ]; then
     echo "RocketLauncher.exe : Found."
-    ./update.sh ${ID} ${PASS}
     ./start.sh ${INSTANCE}
 else
     echo "RocketLauncher.exe : Not Found."
@@ -16,9 +17,8 @@ else
     curl  -O https://ci.rocketmod.net/job/Rocket.Unturned/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip 
     unzip -o Rocket.zip -d ${UNT_DIR}
     rm -f Rocket.zip
-    chmod +x ${UNT_DIR}/Scripts/Linux/*.sh
-    mv ${UNT_DIR}/Scripts/Linux/*.sh ${UNT_DIR}/
+    chmod +x ${UNT_DIR}/Scripts/Linux/start.sh
+    mv ${UNT_DIR}/Scripts/Linux/start.sh ${UNT_DIR}/
     mv ${UNT_DIR}/Scripts/Linux/*.exe ${UNT_DIR}/
-    ./update.sh ${ID} ${PASS}
     ./start.sh ${INSTANCE}
 fi
